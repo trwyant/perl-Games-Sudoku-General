6c4056ca73ad4fb81b0077713fa0bff4e3bce4e3
tom <tom@f6fd3633-1913-47c2-bad4-372d75c948ec>
2009-01-02 17:02:55 +0000

    Bring up to perlcritic --stern.
    git-svn-id: file:///Users/tom/Code/svn/repos/Games-Sudoku-General/trunk@1224 f6fd3633-1913-47c2-bad4-372d75c948ec

--- t/TestDriver.pm
+++ t/TestDriver.pm
@@ -1,13 +1,12 @@
+package t::TestDriver;
 
 use strict;
 use warnings;
 
-package t::TestDriver;
-
 use Games::Sudoku::General;
 use Test;
 
-our $VERSION = '0.001';
+our $VERSION = '0.001_02';
 
 my $method;
 my $number;
@@ -17,89 +16,95 @@ my @todo;
 my %variable;
 
 sub execute {
-my $self = shift;
-my $handle = shift;
+    my $self = shift;
+    my $handle = shift;
 
-my $loc = tell $handle;
+    my $loc = tell $handle;
 
-new ();
+    new ();
 
-for ($pass = 0; $pass < 2; $pass++) {
-    $number = 0;
-    %variable = ();
-    @todo = ();
-    seek ($handle, $loc, 0);
-    while (<$handle>) {
-	s/^\s+//;
-	next unless $_;
-	next if m/^#/;
-	s/\s+$//;
-	my $outside = 1;
-	my @args = map {
-	    $outside++ % 2 ? (map {
-		m/^<<(.+)/ ? do {
-		    my $t = $1 . "\n"; my $r = '';
-		    while (<$handle>) {last if $_ eq $t; $r .= $_}
-		    $r } :
-		m/^\$(.+)/ ? $variable{$1} || $1 :
-		$_
-		} split ('\s+', $_)) :
-	    $_} split "'", $_;
-	$variable{trace} and
-	    print "#> ", join (' ', map {m/\s/ ? "'$_'" : $_} @args), "\n";
-	my $verb = shift @args;
-	if ($verb =~ m/\W/ || $verb =~ m/^_/) {
-	    warn <<eod;
+    for ($pass = 0; $pass < 2; $pass++) {
+	$number = 0;
+	%variable = ();
+	@todo = ();
+	seek ($handle, $loc, 0);
+	while (<$handle>) {
+	    s/^\s+//;
+	    next unless $_;
+	    next if m/^#/;
+	    s/\s+$//;
+	    my $outside = 1;
+	    my @args = map {
+		$outside++ % 2 ? (map {
+		    m/^<<(.+)/ ? do {
+			my $t = $1 . "\n"; my $r = '';
+			while (<$handle>) {last if $_ eq $t; $r .= $_}
+			$r } :
+		    m/^\$(.+)/ ? $variable{$1} || $1 :
+		    $_
+		    } split ('\s+', $_)) :
+		$_} split "'", $_;
+	    $variable{trace} and
+		print "#> ", join (' ', map {m/\s/ ? "'$_'" : $_} @args), "\n";
+	    my $verb = shift @args;
+	    if ($verb =~ m/\W/ || $verb =~ m/^_/) {
+		warn <<eod;
 Error - Verb '$verb' is illegal.
 eod
-	    }
-	  elsif (__PACKAGE__->can ($verb)) {
-	    __PACKAGE__->$verb (@args);
-	    }
-	  elsif ($su->can ($verb)) {
-	    next unless $pass;
-	    $method = $verb;
-	    $variable{result} = eval {$su->$verb (@args)} || $@ || '';
-	    warn $@ if $@;
-	    }
-	  else {
-	    warn <<eod;
+	    } elsif (__PACKAGE__->can ($verb)) {
+		__PACKAGE__->$verb (@args);
+	    } elsif ($su->can ($verb)) {
+		next unless $pass;
+		$method = $verb;
+		$variable{result} = eval {$su->$verb (@args)} || $@ || '';
+		warn $@ if $@;
+	    } else {
+		warn <<eod;
 Error - Verb '$verb' is unknown.
 eod
 	    }
 	}
 
 
-    unless ($pass) {plan tests => $number, todo => \@todo; }
+	unless ($pass) {
+	    plan tests => $number, todo => \@todo;
+	}
 
     }
+    return;
 }
 
 #	echo prints its arguments.
 
 sub echo {
-return unless $pass;
-shift;
-foreach (@_) {
-    chomp;
-    foreach (split '\n', $_) {
-	s/^\.//;
-	print "#         ", ' ' x length ($number), "$_\n" if $_
+    my (undef, @args) = @_;
+    return unless $pass;
+    foreach (@args) {
+	chomp;
+	foreach (split '\n', $_) {
+	    s/^\.//;
+	    print "#         ", ' ' x length ($number), "$_\n" if $_
 	}
     }
+    return;
 }
 
 #	fowler translates Glenn Fowler's cell numbers into mine.
 
 sub fowler {
-$variable{result} = $_[1];
-$variable{result} =~
-    s/\[(\d+),(\d+)\]/'[' . (($1 - 1) * 9 + $2 - 1) . ']'/gem;
+    $variable{result} = $_[1];
+    $variable{result} =~
+	s/\[(\d+),(\d+)\]/'[' . (($1 - 1) * 9 + $2 - 1) . ']'/gem;
+    return;
 }
 
 #	new instantiates a new Games::Sudoku::General object.
 
-sub new {shift; $su = Games::Sudoku::General->new (@_)}
+sub new {
+    shift;
+    $su = Games::Sudoku::General->new (@_);
+    return;
+}
 
 #	test compares the previous result to its first argument. It
 #	also prints some front matter for the test, with the second
@@ -107,46 +112,56 @@ sub new {shift; $su = Games::Sudoku::General->new (@_)}
 #	previous method), and subsequent arguments being echoed.
 
 sub test {
-shift;
-$number++;
-return unless $pass;
-my $expect = shift;
-chomp $expect;
-my $title = shift || $method;
-chomp $title;
-print <<eod;
+    my (undef, $expect, $title, @detail) = @_;
+    $number++;
+    return unless $pass;
+    chomp $expect;
+    $title ||= $method;
+    chomp $title;
+    print <<eod;
 #
 # Test $number - $title
 eod
-__PACKAGE__->echo (@_) if @_;
-my $result = $variable{result};
-chomp $result;
-if ($result =~ m/\n/ || $expect =~ m/\n/) {
-    foreach ([Expect => $expect], [Got => $result]) {
-	printf "# %9s:\n", $_->[0];
-	foreach (split '\n', $_->[1]) {print "#          $_\n"}
+    __PACKAGE__->echo (@detail) if @detail;
+    my $result = $variable{result};
+    chomp $result;
+    if ($result =~ m/\n/ || $expect =~ m/\n/) {
+	foreach ([Expect => $expect], [Got => $result]) {
+	    printf "# %9s:\n", $_->[0];
+	    foreach (split '\n', $_->[1]) {print "#          $_\n"}
 	}
-    }
-  else {
-    print <<eod;
+    } else {
+	print <<eod;
 #    Expect: $expect
 #       Got: $result
 eod
     }
-ok ($expect eq $result);
+    ok ($expect eq $result);
+    return;
 }
 
 #	todo marks the previous test as a 'todo' item.
 
-sub todo {push @todo, $number}
+sub todo {
+    push @todo, $number;
+    return;
+}
 
-sub unload {$variable{result} = $su->_unload () if $pass}
+sub unload {
+    $variable{result} = $su->_unload () if $pass;
+    return;
+}
 
 #	var defines a pseudo-lexical variable, which can be substituted
 #	into the command by prefixing the variable name with a '$'.
 #	Note that $result is implicitly set by any
 #	Games::Sudoku::General method.
 
-sub var {shift; my $name = shift; $variable{$name} = "@_"}
+sub var {
+    shift;
+    my $name = shift;
+    $variable{$name} = "@_";
+    return;
+}
 
 1;
