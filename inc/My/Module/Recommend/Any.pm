e3eecefb545e56fe8ca2879d14e387017c56b5df
Tom Wyant <wyant@cpan.org>
2016-03-01 11:50:16 -0500

    Version 0.018
    Set version to 0.018. Update Changes file for changes since 0.017_02.

--- inc/My/Module/Recommend/Any.pm
+++ inc/My/Module/Recommend/Any.pm
@@ -8,7 +8,7 @@ use warnings;
 use Carp;
 use Exporter qw{ import };
 
-our $VERSION = '0.017_02';
+our $VERSION = '0.018';
 
 our @EXPORT_OK = qw{ __any };
 
--- inc/mock/Clipboard.pm
+++ inc/mock/Clipboard.pm
@@ -7,7 +7,7 @@ use warnings;
 
 use Carp;
 
-our $VERSION = '0.017_02';
+our $VERSION = '0.018';
 
 sub import {
     return;
