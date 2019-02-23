861548d9ef819ad0ad05d4c39ae97c8ccbcad698
Tom Wyant <wyant@cpan.org>
2019-02-23 10:07:15 -0500

    Version 0.022
    Set version to 0.022. Update Changes file for changes since v00.021_01.

--- inc/My/Module/Recommend/Any.pm
+++ inc/My/Module/Recommend/Any.pm
@@ -8,7 +8,7 @@ use warnings;
 use Carp;
 use Exporter qw{ import };
 
-our $VERSION = '0.021_01';
+our $VERSION = '0.022';
 
 our @EXPORT_OK = qw{ __any };
 
--- lib/Games/Sudoku/General.pm
+++ lib/Games/Sudoku/General.pm
@@ -577,7 +577,7 @@ use warnings;
 
 use Exporter qw{ import };
 
-our $VERSION = '0.021_01';
+our $VERSION = '0.022';
 our @EXPORT_OK = qw{
     SUDOKU_SUCCESS
     SUDOKU_NO_SOLUTION
