a031778551309a4d87f1f7e68dab7282cae764b9
Tom Wyant <wyant@cpan.org>
2021-09-07 07:53:55 -0400

    Version 0.026
    Set version to 0.026. Update Changes file for changes since v00.025_01.

--- inc/mock/Clipboard.pm
+++ inc/mock/Clipboard.pm
@@ -7,7 +7,7 @@ use warnings;
 
 use Carp;
 
-our $VERSION = '0.025_01';
+our $VERSION = '0.026';
 
 sub import {
     return;
--- lib/Games/Sudoku/General.pm
+++ lib/Games/Sudoku/General.pm
@@ -574,7 +574,7 @@ use warnings;
 
 use Exporter qw{ import };
 
-our $VERSION = '0.025_01';
+our $VERSION = '0.026';
 our @EXPORT_OK = qw{
     SUDOKU_SUCCESS
     SUDOKU_NO_SOLUTION
