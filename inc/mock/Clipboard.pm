833cbd0a0e2ae3f5460d2d466752797d87c4855f
Tom Wyant <harryfmudd@comcast.net>
2026-06-14 07:15:26 -0400

    Version 0.028
    Set version to 0.028. Update Changes file for changes since v00.027_01.

--- inc/mock/Clipboard.pm
+++ inc/mock/Clipboard.pm
@@ -7,7 +7,7 @@ use warnings;
 
 use Carp;
 
-our $VERSION = '0.027_01';
+our $VERSION = '0.028';
 
 sub import {
     return;
--- lib/Games/Sudoku/General.pm
+++ lib/Games/Sudoku/General.pm
@@ -574,7 +574,7 @@ use warnings;
 
 use Exporter qw{ import };
 
-our $VERSION = '0.027_01';
+our $VERSION = '0.028';
 our @EXPORT_OK = qw{
     SUDOKU_SUCCESS
     SUDOKU_NO_SOLUTION
